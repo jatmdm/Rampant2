95caa598430054212b41f519723ecdbceb30eb1f
jatmdm <jatmdm@gmail.com>
2015-04-11 17:13:32 -0400

    Work hours, nopayy? KIll COnrad

---
+++ Rampant/Assets/Scripts/HandleActors.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HandleActors : MonoBehaviour {
+	public GameObject Player;
+	public List<GameObject> enemies;
+	public float cooldown = 2f;
+	// Use this for initialization
+	void Start () {
+		Player = GameObject.Find("Player");
+		GameObject[] temp = GameObject.FindGameObjectsWithTag("Enemy");
+		getEnemies();
+	}
+
+	void getEnemies(){
+		GameObject[] temp = GameObject.FindGameObjectsWithTag("Enemy");
+		for(int i = 0; i < temp.Length; i++){
+			enemies.Add(temp[i]);
+		}
+	}
+
+	void HandleEnemies(){
+		for(int i = 0; i < enemies.Count; i++){
+			if(Vector2.Distance(enemies[i].transform.position, Player.transform.position) > 10){
+				enemies[i].active = false;
+			}
+			else{
+				enemies[i].active = true;
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		cooldown-=Time.deltaTime;
+		if(cooldown <= 0){
+			getEnemies();
+			HandleEnemies();
+			cooldown = 2f;
+		}
+	}
+}
