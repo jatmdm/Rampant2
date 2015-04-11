61694b19991f4aca8eab4c6abe6449bae3366551
Larry Prince <graham.p.sullivan@gmail.com>
2015-04-11 08:50:55 -0400

    poop

---
+++ Rampant/Assets/Scripts/AI/EnemyAI.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+	
+	public Vector2 targetPosition;
+	public Vector2 dir = Vector2.zero;
+	public Vector2 knock;
+	public float speed;
+
+	public Vector2 minimumGenderTolerance;
+	public Vector2 maximumGenderTolerance;
+
+
+}
