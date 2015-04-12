18f3a6ac0531035addb8491712d1343df6e45a5d
jatmdm <jatmdm@gmail.com>
2015-04-11 20:43:14 -0400

    fixed god

--- Rampant/Assets/Scripts/AI/RevanentAI.cs
+++ Rampant/Assets/Scripts/AI/RevanentAI.cs
@@ -5,10 +5,11 @@ public class RevanentAI : MonoBehaviour {
 
 	public float followDistance;
 	public float chargeTime = 0;
+	public GameObject player;
 
 	// Use this for initialization
 	void Start () {
-
+		player = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	// Update is called once per frame
@@ -16,7 +17,7 @@ public class RevanentAI : MonoBehaviour {
 
 		if(GetComponent<EnemyStats>().dead)
 		{
-			if(transform.eulerAngles.z != 90) GetComponent<EnemyAI>().knock = ((Vector2)transform.position-(Vector2)GameObject.FindGameObjectWithTag ("Player").transform.position).normalized*.05f;
+			if(transform.eulerAngles.z != 90) GetComponent<EnemyAI>().knock = ((Vector2)transform.position-(Vector2)player.transform.position).normalized*.05f;
 			GetComponent<EnemyAI> ().dir = Vector2.zero;
 			GetComponent<EnemyAI>().speed = 0;
 			Destroy(GetComponent<Agent>());
@@ -29,18 +30,17 @@ public class RevanentAI : MonoBehaviour {
 
 		if (!GetComponent<EnemyStats> ().dead) 
 		{
-			if (Vector2.Distance (GameObject.FindGameObjectWithTag ("Player").transform.position, this.gameObject.transform.position) < followDistance &&
-			    GetComponent<EnemyAI>().canSeeObject(GameObject.Find("Player"))) {
-				GetComponent<EnemyAI> ().targetPosition = GameObject.FindGameObjectWithTag ("Player").transform.position;
+			if (Vector2.Distance (player.transform.position, this.gameObject.transform.position) < followDistance) {
+				GetComponent<EnemyAI> ().targetPosition = player.transform.position;
 				GetComponent<EnemyAI> ().speed = Mathf.Lerp (GetComponent<EnemyAI> ().speed, 5, Time.fixedDeltaTime * 5);
 			}
 
-			if (Vector2.Distance (GameObject.FindGameObjectWithTag ("Player").transform.position, this.gameObject.transform.position) < followDistance / 3) {
+			if (Vector2.Distance (player.transform.position, this.gameObject.transform.position) < followDistance / 3) {
 				chargeTime += Time.fixedDeltaTime;
 			}
 
 			if (chargeTime > 2) {
-				GetComponent<EnemyAI> ().dir += ((Vector2)GameObject.FindGameObjectWithTag ("Player").transform.position - (Vector2)transform.position).normalized * 5;
+				GetComponent<EnemyAI> ().dir += ((Vector2)player.transform.position - (Vector2)transform.position).normalized * 5;
 				chargeTime = 0;
 			}
 
@@ -48,11 +48,24 @@ public class RevanentAI : MonoBehaviour {
 			if (GetComponent<EnemyAI> ().targetPosition == null) {
 				GetComponent<EnemyAI> ().dir = Vector2.zero;
 			} else {
+				//Debug.Log("shit");
 				GetComponent<EnemyAI> ().dir = Vector2.Lerp (GetComponent<EnemyAI> ().dir, this.GetComponent<Agent> ().pathing (GetComponent<EnemyAI> ().targetPosition, 1f), Time.fixedDeltaTime * 5);
 			}
 		}
 
 		if(GetComponent<EnemyAI>().knock.magnitude < .1f) GetComponent<Rigidbody2D> ().MovePosition (GetComponent<Rigidbody2D> ().position + GetComponent<EnemyAI>().dir * GetComponent<EnemyAI>().speed * Time.fixedDeltaTime);
 		else GetComponent<Rigidbody2D> ().MovePosition (GetComponent<Rigidbody2D> ().position + GetComponent<EnemyAI>().knock * 20 * Time.fixedDeltaTime);
+	}
+
+	void Update(){
+		if (!GetComponent<EnemyStats> ().dead) 
+		{			
+			if (GetComponent<EnemyAI> ().targetPosition == null) {
+				GetComponent<EnemyAI> ().dir = Vector2.zero;
+			} else {
+				//Debug.Log("shit");
+				GetComponent<EnemyAI> ().dir = Vector2.Lerp (GetComponent<EnemyAI> ().dir, this.GetComponent<Agent> ().pathing (GetComponent<EnemyAI> ().targetPosition, 1f), Time.fixedDeltaTime * 5);
+			}
+		}
 	}
 }
--- Rampant/Assets/Scripts/AI/resonantArcherAI.cs
+++ Rampant/Assets/Scripts/AI/resonantArcherAI.cs
@@ -11,9 +11,12 @@ public class resonantArcherAI : MonoBehaviour {
 	private float weaponDist;
 	private float shoot;
 
+	public GameObject player;
+
 	// Use this for initialization
 	void Start () {
 		bow = Instantiate (bow, transform.position, Quaternion.identity) as GameObject;
+		player = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	// Update is called once per frame
@@ -21,7 +24,7 @@ public class resonantArcherAI : MonoBehaviour {
 
 		if(GetComponent<EnemyStats>().dead)
 		{
-			if(transform.eulerAngles.z != 90) GetComponent<EnemyAI>().knock = ((Vector2)transform.position-(Vector2)GameObject.FindGameObjectWithTag ("Player").transform.position).normalized*.05f;
+			if(transform.eulerAngles.z != 90) GetComponent<EnemyAI>().knock = ((Vector2)transform.position-(Vector2)player.transform.position).normalized*.05f;
 			GetComponent<EnemyAI> ().dir = Vector2.zero;
 			GetComponent<EnemyAI>().speed = 0;
 			Destroy(GetComponent<Agent>());
@@ -36,16 +39,15 @@ public class resonantArcherAI : MonoBehaviour {
 		{
 			GetComponent<EnemyAI> ().dir = Vector2.zero;
 			weaponDist = .65f;
-			Vector2 diff = (GameObject.FindGameObjectWithTag ("Player").transform.position-this.transform.position).normalized;
+			Vector2 diff = (player.transform.position-this.transform.position).normalized;
 			Vector2 dist = new Vector2 (Mathf.Cos (Mathf.Atan2 (diff.y, diff.x)) * weaponDist, Mathf.Sin (Mathf.Atan2 (diff.y, diff.x)) * weaponDist);
 			bow.GetComponent<Rigidbody2D> ().MovePosition ((Vector2)this.transform.position + dist/2);
 			bow.transform.rotation = Quaternion.Euler (0, 0, Mathf.Rad2Deg * Mathf.Atan2 (diff.y, diff.x) - 90);
 
 			shoot -= Time.fixedDeltaTime;
 
-			if (Vector2.Distance (GameObject.FindGameObjectWithTag ("Player").transform.position, this.gameObject.transform.position) < followDistance/3 &&
-			    GetComponent<EnemyAI>().canSeeObject(GameObject.Find ("Player"))) {
-				GetComponent<EnemyAI> ().targetPosition = (this.transform.position-GameObject.FindGameObjectWithTag ("Player").transform.position).normalized*5;
+			if (Vector2.Distance (player.transform.position, this.gameObject.transform.position) < followDistance/3) {
+				GetComponent<EnemyAI> ().targetPosition = (this.transform.position-player.transform.position).normalized*5;
 				GetComponent<EnemyAI> ().speed = Mathf.Lerp (GetComponent<EnemyAI> ().speed, 6, Time.fixedDeltaTime * 5);
 			}
 			else
@@ -57,15 +59,20 @@ public class resonantArcherAI : MonoBehaviour {
 					tmp.GetComponent<arrow>().dir = diff*8;
 				}
 			}
+		}
 
+		if(GetComponent<EnemyAI>().knock.magnitude < .1f) GetComponent<Rigidbody2D> ().MovePosition (GetComponent<Rigidbody2D> ().position + GetComponent<EnemyAI>().dir * GetComponent<EnemyAI>().speed * Time.fixedDeltaTime);
+		else GetComponent<Rigidbody2D> ().MovePosition (GetComponent<Rigidbody2D> ().position + GetComponent<EnemyAI>().knock * 20 * Time.fixedDeltaTime);
+	}
+	void Update(){
+		if (!GetComponent<EnemyStats> ().dead) 
+		{
+			
 			if (GetComponent<EnemyAI> ().targetPosition == null) {
 				GetComponent<EnemyAI> ().dir = Vector2.zero;
 			} else {
 				GetComponent<EnemyAI> ().dir = Vector2.Lerp (GetComponent<EnemyAI> ().dir, this.GetComponent<Agent> ().pathing (GetComponent<EnemyAI> ().targetPosition, 1f), Time.fixedDeltaTime * 5);
 			}
 		}
-
-		if(GetComponent<EnemyAI>().knock.magnitude < .1f) GetComponent<Rigidbody2D> ().MovePosition (GetComponent<Rigidbody2D> ().position + GetComponent<EnemyAI>().dir * GetComponent<EnemyAI>().speed * Time.fixedDeltaTime);
-		else GetComponent<Rigidbody2D> ().MovePosition (GetComponent<Rigidbody2D> ().position + GetComponent<EnemyAI>().knock * 20 * Time.fixedDeltaTime);
 	}
 }
